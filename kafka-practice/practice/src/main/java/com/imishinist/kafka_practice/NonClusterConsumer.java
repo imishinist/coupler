d49904713e1e0e7b36f5a2fb75893eb86a0e5612
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-10 23:39:30 +0900

    get topics from ENV

--- kafka-practice/practice/src/main/java/com/imishinist/kafka_practice/NonClusterConsumer.java
+++ kafka-practice/practice/src/main/java/com/imishinist/kafka_practice/NonClusterConsumer.java
@@ -4,16 +4,17 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class NonClusterConsumer {
     public static void main(String[] args) {
         String bootstrapServers = System.getenv("BOOTSTRAP_SERVERS");
+        String topics = System.getenv("TOPICS");
+
         Properties properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "java-consumer-group");
@@ -22,12 +23,13 @@ public class NonClusterConsumer {
 
         KafkaConsumer<String, String> consumer =
             new KafkaConsumer<>(properties, new StringDeserializer(), new StringDeserializer());
-        consumer.subscribe(Collections.singletonList("my-topic"));
+        consumer.subscribe(Arrays.asList(topics.split(",", 0)));
         try {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(60L));
                 for (ConsumerRecord<String, String> record :records) {
-                    System.out.printf("%s:%s%n", record.offset(), record.value());
+                    System.out.printf("topic: %s, partition: %s, offset: %s, value: %s",
+                            record.topic(), record.partition(), record.offset(), record.value());
                 }
                 consumer.commitSync();
             }
